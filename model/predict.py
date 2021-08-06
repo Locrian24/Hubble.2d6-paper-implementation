8a067fc80b4ccdcf0eb70508cf35e4d7cc47bb01
Locrian24 <locrian24@gmail.com>
2021-08-05 21:34:15 -0700

    evaluation data

--- model/predict.py
+++ model/predict.py
@@ -1,3 +1,28 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from glob import glob
+
+class PredictCYP2D6():
+  def __init__(self, X=None):
+    self.X = X
+    self.predictions = self.predict()
+
+  def predict(self):
+    dir = './ensemble_models/'
+    model_files = glob(dir + "*.model.h5")
+
+    all_predictions = None
+    for file in model_files:
+      model = tf.keras.models.load_model(file)
+      prediction = model.predict(self.X)
+      
+      if all_predictions = None:
+        all_predictions = prediction
+      else:
+        all_predictions.dstack((prediction, all_predictions))
+
+    return all_predictions
+
+if __name__ == "__main__":
+  P = PredictCYP2D6()
